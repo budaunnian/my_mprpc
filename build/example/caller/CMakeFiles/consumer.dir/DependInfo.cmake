7b3a846efd932ea259fffbc5718d2f7585ecfa61
buduan <buduan@example.com>
2026-04-08 22:21:47 +0800

    将rpc远程服务节点存储在zk上

--- build/example/caller/CMakeFiles/consumer.dir/DependInfo.cmake
+++ build/example/caller/CMakeFiles/consumer.dir/DependInfo.cmake
@@ -4,8 +4,8 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/duan/my_rpc/my_mprpc/example/friend.pb.cc" "/home/duan/my_rpc/my_mprpc/build/example/caller/CMakeFiles/consumer.dir/__/friend.pb.cc.o"
-  "/home/duan/my_rpc/my_mprpc/example/caller/callfriendservice.cc" "/home/duan/my_rpc/my_mprpc/build/example/caller/CMakeFiles/consumer.dir/callfriendservice.cc.o"
+  "/home/buduan/ProJect/my_-mqrpc/example/friend.pb.cc" "/home/buduan/ProJect/my_-mqrpc/build/example/caller/CMakeFiles/consumer.dir/__/friend.pb.cc.o"
+  "/home/buduan/ProJect/my_-mqrpc/example/caller/callfriendservice.cc" "/home/buduan/ProJect/my_-mqrpc/build/example/caller/CMakeFiles/consumer.dir/callfriendservice.cc.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
@@ -18,7 +18,7 @@ set(CMAKE_CXX_TARGET_INCLUDE_PATH
 
 # Targets to which this target links.
 set(CMAKE_TARGET_LINKED_INFO_FILES
-  "/home/duan/my_rpc/my_mprpc/build/src/CMakeFiles/mprpc.dir/DependInfo.cmake"
+  "/home/buduan/ProJect/my_-mqrpc/build/src/CMakeFiles/mprpc.dir/DependInfo.cmake"
   )
 
 # Fortran module output directory.
